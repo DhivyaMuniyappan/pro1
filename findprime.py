c46b5bbd4288906c8415f60f71d9fc69ee4d3b59
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-02-20 17:58:18 +0530

    findprime

---
+++ findprime.py
@@ -0,0 +1,17 @@
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+ 
+for num in range(lower+1,upper):
+  
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+ 
+ 
+ 
+ 
+ 
+ 
