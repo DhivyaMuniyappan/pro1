ff60dd32ed1a945568c454a3f1970c6be9fa7541
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-03-05 17:17:26 +0530

    Update limitarmstrong.py

--- limitarmstrong.py
+++ limitarmstrong.py
@@ -3,7 +3,7 @@ upper = int(input("Enter upper range: "))
 for num in range(lower,upper + 1):
 	   sum = 0
 	   temp = num
-	   while temp > 0:
+	   while (temp > 0):
 	       digit = temp % 10
 	       sum += digit ** 3
 	       temp //= 10
